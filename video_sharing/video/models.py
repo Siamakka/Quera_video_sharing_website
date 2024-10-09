d347c9e0110a285a994f7905ee746f44113f53e0
siamak kamanabroo <siamak.kamanabroo@gmail.com>
2024-10-09 13:20:22 +0330

    Create video models

---
+++ video_sharing/video/models.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=255)
+    description = models.TextField(
+        null=True,
+        blank=True,
+        )
+
+    def __str__(self) -> str:
+        return self.category_name
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        )
+    video = models.ForeignKey(
+        'Video',
+        on_delete=models.CASCADE,
+        related_name='comments',
+        )
+    context = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.video.title}"
+
+class Video(models.Model):
+    uploader = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='videos',
+        )
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    video_url = models.URLField(null=False, blank=False)
+    categories = models.ManyToManyField(Category)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    view_counts = models.IntegerField(default=0)
+    like_counts = models.IntegerField(default=0)
+    dislike_counts = models.IntegerField(default=0)
+    is_public = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self) -> str:
+        return self.title
