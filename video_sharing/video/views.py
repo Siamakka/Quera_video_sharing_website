15166e923ef3dfbcace4bf04f108a41144ebfa86
siamak kamanabroo <siamak.kamanabroo@gmail.com>
2024-10-09 14:52:27 +0330

    create RegistrationView in views.py

--- video_sharing/video/views.py
+++ video_sharing/video/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import RegistrationSerializer
+
+class RegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (
+        AllowAny,
+        )
+    serializer_class = RegistrationSerializer
