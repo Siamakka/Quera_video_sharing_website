539c7a39694ec5fd795c75c4ffa7acc2153c2eab
siamak kamanabroo <siamak.kamanabroo@gmail.com>
2024-10-12 20:59:09 +0330

    csrf exempt

--- video_sharing/subscription/views.py
+++ video_sharing/subscription/views.py
@@ -7,8 +7,10 @@ from .models import SubscriptionPlan, Payment
 from django.urls import reverse
 from django.http import JsonResponse
 import uuid
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 
-
+@csrf_exempt
 def upgrade_to_premium(user):
     premium_plan = get_object_or_404(SubscriptionPlan, plan_name='premium')
     subscription = user.subscription
@@ -17,7 +19,7 @@ def upgrade_to_premium(user):
     subscription.end_date = subscription.start_date + timedelta(days=premium_plan.duration_in_days)
     subscription.save()
 
-
+@csrf_exempt
 def create_payment(request, plan_id):
     plan = get_object_or_404(SubscriptionPlan, id=plan_id)
     payment = Payment.objects.create(
@@ -37,6 +39,8 @@ def create_payment(request, plan_id):
         'transaction_id': payment.transaction_id
     })
 
+
+@csrf_exempt
 def confirm_payment(request, transaction_id):
     payment = get_object_or_404(Payment, transaction_id=transaction_id)
 
